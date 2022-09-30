1f8c2e37a6916811e7dcd34b0ed2a8c01971fce8
oriarh <111208485+oriarh@users.noreply.github.com>
2022-09-29 18:20:34 -0700

    pushing the initial code

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 // Assignment code here
 
+function generatePassword() {
+  var criteria = function criteria() {
+    var box = document.createElement("INPUT");                    //Variable LS
+    box.setAttribute("type","checkbox");
+    box.setAttribute("id","accept");
+    document.body.appendChild(box);
+  };
+  prompt(criteria);
+  console.log("Generate Password function is working");
+}
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("generate");          //Variable GS
+
+// Defining the checkbox element
+function checkbox () {
+
+  console.log("Checkbox function is being called");
+};
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  checkbox();
+  var password = generatePassword();                            //Variable LS
+  var passwordText = document.getElementById("password");
+  passwordText.innerHTML = password();
 }
 
 // Add event listener to generate button

a84fa7c40ebfe9606a7c5db773fb371493f8aeb7
oriarh <111208485+oriarh@users.noreply.github.com>
2022-10-04 21:36:21 -0700

    Changed the Javascript to make the password generator work

--- README.md
+++ README.md
@@ -1,25 +1,24 @@
-# Password Generator Starter Code
+# Password Generator
 
-## User Story
+## Descriptions 
+This application generates a random secure password that based on the criteria selected by an employee.
 
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
+## URL
 
-## ACCEPTANCE CRITERIA
+## Screenshot
 
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+
+## Installation
+N/A
+
+## Usage
+When you open this web app, you see a simple user interface. When you click on the generate password button,
+you will be presented with a series of prompts to choose the criteria such as length.
+
+This web app was built using HTML and CSS & Javascript.
+
+## Credits
+UC Berkeley Extension Instructional Staff
+
+## License
+MIT License
--- script.js
+++ script.js
@@ -1,29 +1,60 @@
-// Assignment code here
+//Generate password
+function generatePassword(lengthDataTypeNumber,upperBool,lowerBool,numberBool,specialBool) {
 
-function generatePassword() {
-    var box = document.createElement("INPUT");                    //Variable LS
-    box.setAttribute("type","checkbox");
-    box.setAttribute("id","accept");
-    document.body.appendChild(box);
-    console.log("Generate Password function is working");
-};
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var special = "~`!@#$%^&*()_-+={}[]|;:,<.>/?";
+  var numberss = "0123456789";
+var combination =[]
+
+  if (upperBool) {
+      combination.push(upper)
+    };
+    if (lowerBool) {
+      combination.push(lower)
+    };
+    if (specialBool) {
+      combination.push(special)
+    };
+    if (numberBool) {
+      combination.push(numberss)
+    };
+var combination = combination.join("");
+var combinationArray = combination.split("");
+console.log(combinationArray);
+  var passwordFinal =[];
+for (var i =0; i < lengthDataTypeNumber; i++){
+ passwordFinal.push (combination[Math.floor(Math.random()*combination.length)]);
+ console.log(passwordFinal);
+}
+return passwordFinal.join("");
+}
 
 // Get references to the #generate element
-var generateBtn = document.getElementById("generate");          //Variable GS
-
-// Defining the checkbox element
-function checkbox () {
-
-  console.log("Checkbox function is being called");
-};
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  checkbox();
-  var password = generatePassword();                            //Variable LS
-  var passwordText = document.getElementById("password");
-  passwordText.innerHTML = password;
-}
+   
+  //Creating series of prompts
+  var length = prompt ("What length of the password do you require? Select between 8 to 128");
+  var upperCase = prompt ("Would you like to include UPPER case letters? Yes or No");
+  var lowerCase = prompt ("Would you like to include lower case letters? Yes or No");
+  var specialChars = prompt ("Would you like to include special case letters? Yes or No");
+  var numbers = prompt ("Would you like to include Number case letters? Yes or No");
+  
+  var upperBool = upperCase.toLowerCase() == "yes";
+  var lowerBool = lowerCase.toLowerCase() == "yes";
+  var specialBool = specialChars.toLowerCase() == "yes";
+  var numberBool = numbers.toLowerCase() == "yes";
+  var lengthDataTypeNumber = +length
+
+  var password = generatePassword(lengthDataTypeNumber,upperBool,lowerBool,numberBool,specialBool);
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
